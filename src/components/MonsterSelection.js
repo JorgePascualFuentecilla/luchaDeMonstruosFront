9450642639e79da90091f1934ae003377e617dd3
JorgePascualFuentecilla <jorgepasf@gmail.com>
2025-01-09 15:26:23 +0100

    Estilos y readme añadidos

--- src/components/MonsterSelection.js
+++ src/components/MonsterSelection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { getMonsters } from './api.js';
+import '../assets/styles/styles.css';
+
 // Importa las imágenes locales
 import PokemonRealista from '../assets/images/monsters/PokemonRealista.jpg';
 import PitufoDelKaos from '../assets/images/monsters/PitufoDelKaos.jpg';
@@ -16,32 +18,33 @@ const monsterImages = {
   'MakinaRata': MakinaRata,
 };
 
-const MonsterSelection = function ({ onMonsterSelect}) {
+const MonsterSelection = function ({ onMonsterSelect }) {
   const [monsters, setMonsters] = useState([]);
 
   useEffect(() => {
     getMonsters().then(setMonsters).catch(console.error);
   }, []);
+
   return (
-  <div>
-    <h1>Selecciona un Monstruo</h1>
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {monsters.map((monster) => (
-        <div
-          key={monster.id}
-          style={{ margin: '10px', cursor: 'pointer', textAlign: 'center' }}
-          onClick={() => onMonsterSelect(monster)}
-        >
-          <img
-            src={monsterImages[monster.Nombre]}
-            alt={monster.Nombre}
-            style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-          />
-          <p>{monster.Nombre}</p>
-        </div>
-      ))}
+    <div>
+      <h1>Selecciona un Monstruo</h1>
+      <div className="container">
+        {monsters.map((monster) => (
+          <div
+            key={monster.id}
+            className="card"
+            onClick={() => onMonsterSelect(monster)}
+          >
+            <img
+              src={monsterImages[monster.Nombre]}
+              alt={monster.Nombre}
+            />
+            <p>{monster.Nombre}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default MonsterSelection;
--- src/components/VillainSelection.js
+++ src/components/VillainSelection.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import '../assets/styles/styles.css';
+
 // Importa las imágenes locales
 import tiax from '../assets/images/villains/tiax.png';
 import vampira from '../assets/images/villains/vampira.png';
 import gnomo from '../assets/images/villains/gnomo.png';
 import sauron from '../assets/images/villains/sauron.png';
 import metadona from '../assets/images/villains/metadona.png';
-import {getVillains} from './api.js';
-
+import { getVillains } from './api.js';
 
 const villainImages = {
   'Tiax': tiax,
@@ -17,32 +18,33 @@ const villainImages = {
   'MetaDonna': metadona,
 };
 
-const VillainSelection = function ({ onVillainSelect}) { 
+const VillainSelection = function ({ onVillainSelect }) {
   const [villains, setVillains] = useState([]);
 
   useEffect(() => {
     getVillains().then(setVillains).catch(console.error);
-  }, [])
+  }, []);
+
   return (
-  <div>
-    <h1>Selecciona un Villano</h1>
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {villains.map((villain) => (
-        <div
-          key={villain.id}
-          style={{ margin: '10px', cursor: 'pointer', textAlign: 'center' }}
-          onClick={() => onVillainSelect(villain)}
-        >
-          <img
-            src={villainImages[villain.Nombre]}
-            alt={villain.Nombre}
-            style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-          />
-          <p>{villain.Nombre}</p>
-        </div>
-      ))}
+    <div>
+      <h1>Selecciona un Villano</h1>
+      <div className="container">
+        {villains.map((villain) => (
+          <div
+            key={villain.id}
+            className="card"
+            onClick={() => onVillainSelect(villain)}
+          >
+            <img
+              src={villainImages[villain.Nombre]}
+              alt={villain.Nombre}
+            />
+            <p>{villain.Nombre}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default VillainSelection;
